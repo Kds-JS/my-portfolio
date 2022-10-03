c82f1a58af9c1661b6508e3d5bbf733734d9342b
kds <kdskoudous98@gmail.com>
2022-10-03 17:13:55 +0100

    update

--- Components/About/About.tsx
+++ Components/About/About.tsx
@@ -58,7 +58,7 @@ const About = () => {
                 borderRadius='md'
                 boxSize={{base:'90%', md:'100%'}}
                 src='https://i.ibb.co/HTkp50p/avataaars.png'
-                alt='Dan Abramov'
+                alt='profil'
                 />
                 </Center>
             </GridItem>
@@ -67,13 +67,12 @@ const About = () => {
 
                 <Box fontSize="20px">
                 <Text >
-                Je m'appelle Koudous et je suis développeur web Front-End React JS dépuis le Bénin.
+                {"Je m'appelle Koudous et je suis développeur web Front-End React JS dépuis le Bénin."}
                 </Text>
 
                 <Text my="20px">Je suis autodidacte et passionné par les dernières technologies web notamment la Blockchain.</Text>
 
-                <Text>Après un bootcamp de 4 mois où j'ai appris (HTML, CSS, Bootstrap, JavaScript, PHP et MySQL), j'ai continué à me former en autodidacte. Je suis toujours intéressé par l'apprentissage de
-                nouvelles technologies et de nouveaux langages.</Text>
+                <Text>{"Après un bootcamp de 4 mois où j'ai appris (HTML, CSS, Bootstrap, JavaScript, PHP et MySQL), j'ai continué à me former en autodidacte. Je suis toujours intéressé par l'apprentissage de nouvelles technologies et de nouveaux langages."}</Text>
                 </Box>
 
 
