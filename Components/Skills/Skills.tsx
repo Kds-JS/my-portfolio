9e9e5a05ce9aa22e483379c67c6440d04ca85f5d
kds <kdskoudous98@gmail.com>
2023-01-01 15:22:25 +0100

    update skill section

--- Components/Skills/Skills.tsx
+++ Components/Skills/Skills.tsx
@@ -7,6 +7,9 @@ import { GrReactjs, GrNode} from 'react-icons/gr';
 import { TbBrandNextjs} from 'react-icons/tb';
 import { FaHardHat} from 'react-icons/fa';
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Skills = () => {
 
@@ -77,42 +80,94 @@ const Skills = () => {
         },
     ]
 
+    var settings = {
+        // dots: false,
+        // infinite: false,
+        // speed: 500,
+        // slidesToShow: 6,
+        // slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2500,
+      autoplaySpeed: 100,
+      cssEase: "linear",
+        responsive: [
+          {
+            breakpoint: 1290,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 950,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      };
+
     return (
         <Box py="40px" id="skills">
         <Box textAlign="center" >
             <Heading color="white" size="2xl">Skills</Heading>
         </Box>
 
-        <Box  py="100px">
+        <Box  pt="100px" >
 
-            <Grid templateColumns={{base: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)', xl: 'repeat(6, 1fr)'}} gap={10}>
+            {/* <Grid templateColumns={{base: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)', xl: 'repeat(6, 1fr)'}} gap={10}> */}
 
-
+            <Slider {...settings} overflow="visible">
             {skills.map((item, index) => (
-                <Flex height="180px" width={{base: "100%", md: "100%"}} bg="secondary.900" flexDirection="column" align="center" py="25px" justify="space-between" borderRadius="md" boxShadow='dark-lg' transform="scale(1)"
+                <Box height="180px" bg="secondary.900" borderRadius="lg!important" boxShadow="dark-lg!important" transform="scale(1)"
                 transition= "0.3s ease-in-out" _hover={{
-                    transform: "scale(1.15)"
-                }} key={index}>
-                    <Box fontSize="60px" color={item.color}>
+                    transform: "scale(1.15)",
+                    // boxShadow:'dark-lg!important'
+                }} key={index} marginX="25px" className='skill-box'>
+                    <Box color={item.color} className="box-image">
                         <item.img/>
                     </Box>
-                    <Text fontSize="18px">{item.title}</Text>
-                </Flex>
+                    <Text >{item.title}</Text>
+                </Box>
+                
             ))}
 
-            <Flex height="180px" width={{base: "100%", md: "100%"}} bg="secondary.900" flexDirection="column" align="center" py="25px" justify="space-between" borderRadius="md" boxShadow='dark-lg' transform="scale(1)"
+            <Box height="180px" bg="secondary.900" borderRadius="lg!important" boxShadow="dark-lg!important" transform="scale(1)"
                 transition= "0.3s ease-in-out" _hover={{
-                transform: "scale(1.15)"
-            }}>
-                <Box>
-                    <Image src='https://i.ibb.co/7J6KBF8/ethers-logo.png' alt="ethers-js" h="60px"/>
+                    transform: "scale(1.15)"
+                }} marginX="25px" className='skill-box'>
+                    <Box className="box-image">
+                        <Image src='https://i.ibb.co/7J6KBF8/ethers-logo.png' alt="ethers-js" h="60px"/>
+                    </Box>
+                    <Text >Ethers JS</Text>
                 </Box>
-                <Text fontSize="18px">Ethers JS</Text>
-            </Flex>
+
+            
 
             
             
-            </Grid>
+            </Slider>
+            {/* </Grid> */}
             
         </Box>
 
